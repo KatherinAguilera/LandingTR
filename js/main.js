ac3c36fd752809491f642557c87c35efe0128026
USERSAD\jaguilem <jaguilem@emeal.nttdata.com>
2022-02-28 12:58:37 -0500

    test

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+/****************** MODAL *******************/
+
+const openModal = document.querySelectorAll("[data-open]");
+const closeModal = document.querySelectorAll("[data-close]");
+const isVisible = "is-visible";
+
+for (const el of openModal) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
+for (const el of closeModal) {
+  el.addEventListener("click", function() {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+  });
+}
+console.log(closeModal);
+document.addEventListener("click", e => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+document.addEventListener("keyup", e => {
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+/***************** MENU ********************/
+
+const $boton = document.getElementById('boton');
+const $menu = document.getElementById('menu');
+
+$boton.addEventListener('click', (event) => {
+  console.log(event);
+  $menu.classList.toggle('active1');
+});
